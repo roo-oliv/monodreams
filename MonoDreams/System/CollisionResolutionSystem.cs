9a12f92d2f2a943fc71d7a98059d61ff29d92cd5
Rodrigo Martins Higuti de Oliveira <rodrigo.oliveira@quintoandar.com.br>
2022-12-28 22:26:09 -0300

    Collisions in order

--- MonoDreams/System/CollisionResolutionSystem.cs
+++ MonoDreams/System/CollisionResolutionSystem.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using DefaultEcs;
 using DefaultEcs.System;
-using Microsoft.Xna.Framework;
 using MonoDreams.Component;
 using MonoDreams.Message;
 using MonoDreams.State;
@@ -31,42 +29,34 @@ public class CollisionResolutionSystem : ISystem<GameState>
 
     public void Update(GameState state)
     {
-        if (_collisions.Count == 0)
-        {
-            return;
-        }
-            
         _collisions.Sort((l, r) => l.ContactTime.CompareTo(r.ContactTime));
         foreach (var collision in _collisions)
         {
-            var entity = collision.BaseEntity;
-            ref var dynamicRect = ref entity.Get<DrawInfo>().Destination;
-            ref var body = ref entity.Get<DynamicBody>();
-            var displacement = body.NextPosition - body.CurrentPosition;
-            ref var targetRect = ref collision.CollidingEntity.Get<DrawInfo>().Destination;
-            if (!CollisionDetectionSystem.DynamicRectVsRect(dynamicRect, displacement, targetRect,
-                    out var contactPoint, out var contactNormal, out var contactTime)) continue;
-            if (contactNormal.X != 0)
-            {
-                // position.ArtificialIncrement(contactPoint.X - dynamicRect.Width / 2f - position.CurrentValue.X, 0);
-                // position.UpdateValue(new (contactPoint.X - dynamicRect.Width / 2f, position.CurrentValue.Y));
-                body.NextPosition.X = contactPoint.X - dynamicRect.Width / 2f;
-                body.CurrentPosition.X = body.NextPosition.X;
-            }
-            if (contactNormal.Y != 0)
-            {
-                // position.ArtificialIncrement(0, contactPoint.Y - dynamicRect.Height / 2f - position.CurrentValue.Y);
-                // position.UpdateValue(new (position.CurrentValue.X, contactPoint.Y - dynamicRect.Height / 2f));
-                body.NextPosition.Y = contactPoint.Y - dynamicRect.Height / 2f;
-                body.CurrentPosition.Y = body.NextPosition.Y;
-            }
-
-            if (contactNormal.Y < 0)
-            {
-                body.IsRiding = true;
-            }
-            //entity.Remove<Collision>();
+            ResolveCollision(collision);
         }
         _collisions.Clear();
+    }
+
+    private static void ResolveCollision(CollisionMessage collision)
+    {
+        var entity = collision.BaseEntity;
+        ref var dynamicRect = ref entity.Get<DrawInfo>().Destination;
+        ref var body = ref entity.Get<DynamicBody>();
+        var displacement = body.NextPosition - body.CurrentPosition;
+        ref var targetRect = ref collision.CollidingEntity.Get<DrawInfo>().Destination;
+        if (!CollisionDetectionSystem.DynamicRectVsRect(dynamicRect, displacement, targetRect,
+                out var contactPoint, out var contactNormal, out var contactTime)) return;
+        if (contactNormal.X != 0)
+        {
+            body.NextPosition.X = contactPoint.X - dynamicRect.Width / 2f;
+            body.CurrentPosition.X = body.NextPosition.X;
+        }
+
+        if (contactNormal.Y != 0)
+        {
+            body.NextPosition.Y = contactPoint.Y - dynamicRect.Height / 2f;
+            body.CurrentPosition.Y = body.NextPosition.Y;
+            body.IsRiding |= contactNormal.Y < 0;
+        }
     }
 }
